c6f149f795b068dcca955c227ed29882a123f3fc
bvi-github <bvi.email@gmail.com>
2010-11-29 22:07:33 +0300

    some footer and style.css changes

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
 <div id="footer">
 	<div class="wrapper clear">
 		<div id="copyright">
-			<p class="copyright-notice"><?php _e('Copyright', 'titan'); ?> &copy; <?php echo date('Y'); ?> <?php echo $titan->copyrightName(); ?>. <a href="http://thethemefoundry.com/titan/">Titan Theme</a> by <a href="http://thethemefoundry.com">The Theme Foundry</a>.</p>
+			<p class="copyright-notice"><?php _e('Copyright', 'titan'); ?> &copy; <?php echo date('Y'); ?> <?php echo $titan->copyrightName(); ?>. </p>
 		</div><!--end copyright-->
 	</div><!--end wrapper-->
 </div><!--end footer-->
