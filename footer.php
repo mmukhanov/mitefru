ca64cd313d879ed2a38acedb6bbe04a8385e4878
bvi-github <bvi.email@gmail.com>
2010-11-30 01:31:56 +0300

    работаю с footer. Убрал все лишнее. Попытался добавить навигационную панель (Карта сайта|Контакты|Ссылки) Пока не получилось поместить их по центру...

--- footer.php
+++ footer.php
@@ -3,6 +3,13 @@
 </div><!--end content-background-->
 <div id="footer">
 	<div class="wrapper clear">
+		<div id="footer_navpanel">
+			<dl>
+				<dd>Карта сайта</dd>
+				<dd>Контакты</dd>
+				<dd>Ссылки</dd>
+			</dl>
+		</div> <!--end footer_navpanel -->
 		<div id="copyright">
 			<p class="copyright-notice"><?php _e('Copyright', 'titan'); ?> &copy; <?php echo date('Y'); ?> <?php echo $titan->copyrightName(); ?>. </p>
 		</div><!--end copyright-->
